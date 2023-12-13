34470a4cfa7a9c375b24278b7d5bc79aed1397d4
Abe <aoejile@mit.edu>
2023-12-12 20:35:27 -0500

    final iteration of project

--- bin/dockerhub_run.sh
+++ bin/dockerhub_run.sh
@@ -4,4 +4,4 @@ if [ -f "$FILE" ]; then
 fi
 docker run --rm -v "$PWD:/srv/jekyll/" -p "8080:8080" \
                     -it amirpourmand/al-folio:v0.7.0 bundler  \
-                    exec jekyll serve --watch --port=8080 --host=0.0.0.0 
+                    exec jekyll serve --watch --port=8080 --host=0.0.0.0
